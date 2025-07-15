20524ce7894c7467e3f404a20d342bcafcdff46e
b1nary0mega <b1nary0mega@users.noreply.github.com>
2025-07-15 14:49:01 -0400

    Update update.sh

--- debian/update.sh
+++ debian/update.sh
@@ -31,8 +31,8 @@ if [ ! -f /opt/ohmyzsh.sh ]; then
     if [ -n "$SUDO_USER" ]; then
         sudo -u "$SUDO_USER" sh /opt/ohmyzsh.sh --unattended
         echo ".. ${BLUE}Updating plugins and theme(s) for $SUDO_USER.${NC}"
-        sudo -u "$SUDO_USER" sed -i 's/^plugins=(git)$/plugins=(git debian ansible colorize)/' "~$SUDO_USER/.zshrc"
-        sudo -u "$SUDO_USER" sed -i 's/^ZSH_THEME="robbyrussell"$/ZSH_THEME="aussiegeek"/' "~$SUDO_USER/.zshrc"
+        sudo -u "$SUDO_USER" sed -i 's/^plugins=(git)$/plugins=(git debian ansible colorize)/' ~$SUDO_USER/.zshrc
+        sudo -u "$SUDO_USER" sed -i 's/^ZSH_THEME="robbyrussell"$/ZSH_THEME="aussiegeek"/' ~$SUDO_USER/.zshrc
     fi
 fi
 
